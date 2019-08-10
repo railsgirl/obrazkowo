a56e0772a0c5cc87c20dc5a7e041ee309484db2b
Paulina <paulina.recko@gmail.com>
2019-08-10 13:02:32 +0200

    Enable adding pictures with Active Storage

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -42,7 +42,7 @@ class PinsController < ApplicationController
   private
 
   def pin_params
-    params.require(:pin).permit(:description)
+    params.require(:pin).permit(:description,:image)
   end
 
   def find_pin
